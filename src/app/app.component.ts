f8a37a58c3809d81b05cb6a4fe78096763b25064
Manishthakur1297 <Manishthakur1297@gmail.com>
2019-07-13 02:26:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PipeDemo';
+
+  user = 
+  {
+    "name" : "john wick",
+    "age" : "20",
+    "branch" : "CSE",
+    "dob" : new Date(1999, 8, 22),
+    "college" : "iit",
+    "rollno" : "3SJ567CS2019"
+  }
+
 }
